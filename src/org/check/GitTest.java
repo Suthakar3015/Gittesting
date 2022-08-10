08046dfeab05c4bd4a9a9cd994a803c5f15ba19f
unknown <suthakar.kamu@gmail.com>
2022-08-10 14:13:46 +0530

    added new updated

--- src/org/check/GitTest.java
+++ src/org/check/GitTest.java
@@ -8,28 +8,10 @@ public class GitTest {
 	System.out.println("12345");
 	System.out.println("aaaaaa");
 	System.out.println("hksuhufiu");
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	System.out.println("1");
+	System.out.println("2");
+	System.out.println("3");
+			
 	}
 
 }
